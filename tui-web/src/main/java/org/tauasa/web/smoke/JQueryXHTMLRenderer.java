c97aba1bc7b0d8d9fc2a78d6fbda947b5c24ec14
Tauasa Timoteo <tauasa@gmail.com>
2024-06-05 12:40:51 -0700

    Update copyright notice; add MIT License text

--- tui-web/src/main/java/org/tauasa/web/smoke/JQueryXHTMLRenderer.java
+++ tui-web/src/main/java/org/tauasa/web/smoke/JQueryXHTMLRenderer.java
@@ -1,5 +1,24 @@
-/**
- * Copyright (c) 2012 Tauasa Timoteo. All rights reserved.
+/*
+ * Copyright 2012 Tauasa Timoteo
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+ * software and associated documentation files (the “Software”), to deal in 
+ * the Software without restriction, including without limitation the rights to use, 
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
+ * the Software, and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE 
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+ * OTHER DEALINGS IN THE SOFTWARE.
  */
 package org.tauasa.web.smoke;
 
@@ -224,7 +243,7 @@ public class JQueryXHTMLRenderer implements ISmoketestResultsRenderer {
 				printProperties(XProperties.loadProperties(getClass().getClassLoader().getResourceAsStream(buildPropertiesPath)),
 						"Build Info ["+(isTrue(DOWNLOAD_RESOURCES_KEY)?buildPropLink:buildPropertiesPath)+"]",
 						null, false);
-			}catch(Exception e){
+			}catch(IOException e){
 				logger.warn("Problem printing build file \""+buildPropertiesPath+"\"", e);
 			}
 		}
@@ -396,6 +415,7 @@ public class JQueryXHTMLRenderer implements ISmoketestResultsRenderer {
 
 				//sort messages by timestamp
 				Collections.sort(result, new Comparator<SmoketestMessage>(){
+                                        @Override
 					public int compare(SmoketestMessage o1, SmoketestMessage o2){
 						return o1.getTimestamp().compareTo(o2.getTimestamp());
 					}
